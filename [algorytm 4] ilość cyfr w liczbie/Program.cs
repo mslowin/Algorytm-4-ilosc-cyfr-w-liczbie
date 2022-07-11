9d16ce77eea07dd654d6a74d66806100c57b44d9
mslowin <slowinski.michal@outlook.com>
2022-07-11 10:43:58 +0200

    Dodaj pliki projektów.

---
+++ [algorytm 4] ilość cyfr w liczbie/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+bool run= true;
+List<char> digits = new List<char>();
+
+
+Console.WriteLine("Enter an intiger. q to exit.\n");
+
+
+while (run)
+{
+    string number = Console.ReadLine();
+    if (number == "q")
+    {
+        run = false;
+    }
+    else if (int.TryParse(number, out int value)) // if the input is an intiger
+    {
+        digits.AddRange(number);
+        Console.WriteLine("Number of all digits: " + digits.Count);
+        digits = digits.Distinct().ToList();  //getting rid of duplicates
+        Console.WriteLine("Number of digits but duplicates doesn't count: " + digits.Count);
+        digits.Clear();
+
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("The input must be an intiger");
+    }
+    Console.WriteLine();
+}
+
+
